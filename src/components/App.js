9cb0f9ec51f051ed219121a6cbf259e93a02d43e
Brandon Zeng <bzeng1234@gmail.com>
2023-04-28 00:25:45 -0400

    fix current count sync and shuffle card

--- src/components/App.js
+++ src/components/App.js
@@ -28,39 +28,69 @@ function App() {
     {
       id: 6,
       clickCount: 0
+    },
+    {
+      id: 7,
+      clickCount: 0
+    },
+    {
+      id: 8,
+      clickCount: 0
+    },
+    {
+      id: 9,
+      clickCount: 0
+    },
+    {
+      id: 10,
+      clickCount: 0
     }
   ]
+
   const [cardStruct, setCardStruct] = useState(initCardStruct);
   const [currentScore, setCurrentScore] = useState(0);
-
-  useEffect(() => {
-    console.log('card clicked');
-    let newScore = currentScore + 1;
-    setCurrentScore(newScore);
-  }, [cardStruct, setCurrentScore]);
-
-
-  useEffect(() => {
-    console.log('score change')
-  }, [currentScore]);
-
   const [bestScore, setBestScore] = useState(0);
-  const [pressedTwice, setPressedTwice] = useState(false);
+  let pressedTwice = false;
 
   useEffect(() => {
-  }, [pressedTwice]);
+    function shuffle(array) {
+      let currentIndex = array.length,  randomIndex;
+      // While there remain elements to shuffle.
+      while (currentIndex !== 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+      return array;
+    }
+    setCardStruct(shuffle(cardStruct));
+  }, [cardStruct]);
 
   function handleCard(i) {
     let arr = cardStruct.slice();
     arr.forEach((elem) => {
-      if(elem.id === i) {
+      if(elem.id === i && elem.clickCount !== 1) {
         elem.clickCount++;
+        return;
+      } else if (elem.id === i && elem.clickCount === 1) {
+        pressedTwice = true;
         return;
       }
     });
-
+    
+    if(!pressedTwice) {
+      let newScore = currentScore + 1;
+      setCurrentScore(newScore);
       setCardStruct(arr);
-      
+    } else {
+      setBestScore(currentScore);
+      setCurrentScore(0);
+      setCardStruct(initCardStruct);
+      pressedTwice = false;
+    }
   }
 
   return (
